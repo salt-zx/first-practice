278295fa6a169dc1f5be0a7351d30f2ff875d9b0
salt-zx <xbz-zwz21@qq.com>
2026-08-07 16:19:59 +0800

    current_led 1->2重改

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -89,7 +89,7 @@ int main(void)
 
   /* USER CODE BEGIN 1 */
   /* 局部变量：只能在 main 函数内使用 */
-  uint8_t  current_led = 2U;              /* 当前要操作的 LED 编号 */
+  uint8_t  current_led = 1U;              /* 当前要操作的 LED 编号 */
   uint16_t blink_times = BLINK_TIMES;/* 每颗 LED 闪烁次数 */
   uint32_t delay_ms    = DELAY_MS;   /* 亮/灭延时 */
   const uint8_t led_count = LED_COUNT; /* const 表示该变量不允许修改 */
@@ -128,7 +128,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    current_led = 1U; /* 练习点：改成 2U，观察从哪颗 LED 开始 */
+    current_led = 2U; /* 练习点：改成 2U，观察从哪颗 LED 开始 */
 
     /* while 循环：条件成立就反复执行 {} 里的代码 */
     while (current_led <= led_count)

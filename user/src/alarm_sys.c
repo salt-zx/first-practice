16f3f0ad76a3b70cddc2ae4e046c6bd2cc8b3814
salt-zx <xbz-zwz21@qq.com>
2026-08-08 14:36:44 +0800

    第二次尝试(补充)

--- user/src/alarm_sys.c
+++ user/src/alarm_sys.c
@@ -2,42 +2,29 @@
 
 static alarm_sys_mode current_mode = IDEL;
 static uint32_t last_blink_time = 0;
- void alarm_sys_func(alarm_sys_mode target_mode)
+void alarm_sys_func(alarm_sys_mode target_mode)
 {
-    if (current_mode != target_mode)
+    if (target_mode != current_mode)
     {
         current_mode = target_mode;
-        last_blink_time = HAL_GetTick();
+        HAL_GPIO_WritePin(LED_GPIO_PORT, LED1_PIN | LED2_PIN | LED3_PIN | LED4_PIN, GPIO_PIN_RESET);//用于关闭所有的led
+        HAL_GPIO_WritePin(BUZZER_GPIO_PORT, BUZZER_PIN, GPIO_PIN_RESET);//关闭蜂鸣器
     }
-
     switch (current_mode)
     {
-        case IDEL:
-            led_off();
-            buzzer_off();
-            break;
+    case IDEL:
+        HAL_GPIO_WritePin(LED_GPIO_PORT, LED1_PIN | LED2_PIN | LED3_PIN | LED4_PIN, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(BUZZER_GPIO_PORT, BUZZER_PIN, GPIO_PIN_RESET);
+        break;
+    case RUN:
+        led_blink_func(250);
+        HAL_GPIO_WritePin(BUZZER_GPIO_PORT, BUZZER_PIN, GPIO_PIN_RESET);
+        break;
+    case ALARM:
+        led_blink_func(50);
 
-        case RUN:
-            if (HAL_GetTick() - last_blink_time >= 500)
-            {
-                led_on();
-                last_blink_time = HAL_GetTick();
-            }
-
-            buzzer_off();
-            break;
-
-        case ALARM:
-            if (HAL_GetTick() - last_blink_time >= 50)
-            {
-                led_on();
-                last_blink_time = HAL_GetTick();
-            }
-
-            buzzer_on();
-            break;
-
-        default:
-            break;
+        HAL_GPIO_TogglePin(BUZZER_GPIO_PORT, BUZZER_PIN);
+        last_time = HAL_GetTick();
     }
+}
 }
